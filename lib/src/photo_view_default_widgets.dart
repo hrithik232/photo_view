ce6052eed3f210c9221ca0df4d2621ced9cc9d4d
hrithik <hrithik23@users.noreply.github.com>
2021-12-19 01:45:45 +0530

    changes done

--- lib/src/photo_view_default_widgets.dart
+++ lib/src/photo_view_default_widgets.dart
@@ -28,17 +28,11 @@ class PhotoViewDefaultLoading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final expectedBytes = event?.expectedTotalBytes;
-    final loadedBytes = event?.cumulativeBytesLoaded;
-    final value = loadedBytes != null && expectedBytes != null
-        ? loadedBytes / expectedBytes
-        : null;
-
     return Center(
       child: Container(
         width: 20.0,
         height: 20.0,
-        child: CircularProgressIndicator(value: value),
+        child: CircularProgressIndicator(),
       ),
     );
   }
